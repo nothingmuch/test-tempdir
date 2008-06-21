59acec8cecb1757b7522b54f70580bbfa5ace273
Yuval Kogman <nothingmuch@woobling.org>
2008-06-21 23:45:38 +0000

    main docs

--- lib/Test/TempDir.pm
+++ lib/Test/TempDir.pm
@@ -52,13 +52,80 @@ __END__
 
 =head1 NAME
 
-Test::TempDir - 
+Test::TempDir - Temporary files support for testing.
 
 =head1 SYNOPSIS
 
-	use Test::TempDir qw(tempdir tempfile scratch);
+	use Test::TempDir;
+
+	my $test_tempdir = temp_root();
+
+	my ( $fh, $file ) = tempfile();
+
+	my $directory_scratch_obj = scratch();
 
 =head1 DESCRIPTION
+
+Test::TempDir provides temporary directory creation with testing in mind.
+
+The differences between using this and using L<File::Temp> are:
+
+=over 4
+
+=item *
+
+If C<t/tmp> is available (writable, creatable, etc) it's preferred over
+C<$ENV{TMPDIR}> etc. Otherwise a temporary directory will be used.
+
+This is C<temp_root>
+
+=item *
+
+Lockfiles are used on C<t/tmp>, to prevent race conditions when running under a
+parallel test harness.
+
+=item *
+
+The C<temp_root> is cleaned at the end of a test run, but not if tests failed.
+
+=item *
+
+C<temp_root> is emptied at the begining of a test run unconditionally.
+
+=item *
+
+The default policy is not to clean the individual C<tempfiles> and C<tempdirs>
+within C<temp_root>, in order to aid in debugging of failed tests.
+
+=back
+
+=head1 EXPORTS
+
+=over 4
+
+=item temp_root
+
+The root of the temporary stuff.
+
+=item tempfile
+
+=item tempdir
+
+Wrappers for the L<File::Temp> functions of the same name.
+
+The default options are changed to use C<temp_root> for C<DIR> and disable
+C<CLEANUP>, but these are overridable.
+
+=item scrach
+
+Loads L<Directory::Scratch> and instantiates a new one, with the same default
+options as C<tempfile> and C<tempdir>.
+
+=back
+
+=head1 SEE ALSO
+
+L<File::Temp>, L<Directory::Scratch>, L<Path::Class>
 
 =cut
 
