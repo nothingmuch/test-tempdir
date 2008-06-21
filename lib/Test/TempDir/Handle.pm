16b70b7876aab2d444c35fc50c684b7e407b5e7f
Yuval Kogman <nothingmuch@woobling.org>
2008-06-21 22:43:46 +0000

    Test::TempDir::Handle

---
+++ lib/Test/TempDir/Handle.pm
@@ -0,0 +1,125 @@
+#!/usr/bin/perl
+
+package Test::TempDir::Handle;
+use Moose;
+
+use MooseX::Types::Path::Class qw(Dir);
+use Moose::Util::TypeConstraints;
+
+use namespace::clean -except => [qw(meta)];
+
+has dir => (
+	isa => Dir,
+	is  => "ro",
+	handles => [qw(file subdir rmtree)],
+);
+
+has lock => (
+	isa => "File::NFSLock",
+	is  => "ro",
+	predicate => "has_lock",
+	clearer   => "clear_lock",
+);
+
+has cleanup_policy => (
+	isa => enum( __PACKAGE__ . "::CleanupPolicy", qw(success always never) ),
+	is  => "rw",
+	default => "success",
+);
+
+has test_builder => (
+	isa => "Test::Builder",
+	is  => "rw",
+	lazy_build => 1,
+	handles => { test_summary => "summary" },
+);
+
+sub _build_test_builder {
+	require Test::Builder;
+	Test::Builder->new;
+}
+
+sub failing_tests {
+	my $self = shift;
+	grep { !$_ } $self->test_summary;
+}
+
+sub empty {
+	my $self = shift;
+	return unless -d $self->dir;
+	warn "will empty " . $self->dir;
+	#$self->rmtree({ keep_root => 1 });
+}
+
+sub delete {
+	my $self = shift;
+	return unless -d $self->dir;
+	warn "will delete " . $self->dir;
+	#$self->rmtree({ keep_root => 0 });
+}
+
+sub release_lock {
+	my $self = shift;
+
+	$self->clear_lock;
+
+	# FIXME always unlock? or allow people to keep the locks around by enrefing them?
+
+	#if ( $self->has_lock ) {
+	#	$self->lock->unlock;
+	#	$self->clear_lock;
+	#}
+}
+
+sub DEMOLISH {
+	my $self = shift;
+	$self->cleanup;
+}
+
+sub cleanup {
+	my ( $self, @args ) = @_;
+
+	$self->release_lock;
+
+	my $policy = "cleanup_policy_" . $self->cleanup_policy;
+
+	$self->can($policy) or die "Unknown cleanup policy " . $self->cleanup_policy;
+
+	$self->$policy(@args);
+}
+
+sub cleanup_policy_never {}
+
+sub cleanup_policy_always {
+	my ( $self, @args ) = @_;
+
+	$self->delete;
+}
+
+sub cleanup_policy_success {
+	my ( $self, @args ) = @_;
+
+	return if $self->failing_tests;
+
+	$self->cleanup_policy_always(@args);
+}
+
+__PACKAGE__
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Test::TempDir::Handle - 
+
+=head1 SYNOPSIS
+
+	use Test::TempDir::Handle;
+
+=head1 DESCRIPTION
+
+=cut
+
+
---
+++ t/handle.t
@@ -0,0 +1,55 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Test::More 'no_plan';
+
+use Path::Class;
+use File::Temp qw(tempdir);
+use File::Path;
+
+use ok 'Test::TempDir::Handle';
+
+my $tmp = dir( tempdir( CLEANUP => 1 ) );
+
+isa_ok( my $h = Test::TempDir::Handle->new( dir => $tmp ), "Test::TempDir::Handle" );
+
+is( $h->dir, $tmp, "dir set" );
+
+is( $h->cleanup_policy, "success", "default cleanup policy" );
+
+my $file = $h->dir->file("foo");
+my $subdir = $h->dir->subdir("bar");
+
+$file->touch;
+$subdir->mkpath;
+
+ok( -f $file, "file created" );
+ok( -d $subdir, "subdir created" );
+
+$h->empty;
+
+ok( not(-f $file), "file removed by empty" );
+ok( not(-d $subdir), "subdir removed by empty" );
+
+is_deeply( [ $h->dir->children ], [], "no children" );
+
+ok( -d $tmp, "dir exists" );
+
+$file->touch;
+
+ok( -f $file, "file exists" );
+
+$h->cleanup_policy("never");
+
+$h->cleanup;
+
+ok( -f $file, "file exists" );
+
+$h->cleanup_policy("always");
+
+$h->cleanup;
+
+ok( not(-d $tmp), "dir removed by delete" );
+
