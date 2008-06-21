13755cde80344eb1b1b319939ade94f27d94bd4e
Yuval Kogman <nothingmuch@woobling.org>
2008-06-21 22:44:10 +0000

    Test::TempDir

---
+++ lib/Test/TempDir.pm
@@ -0,0 +1,67 @@
+#!/usr/bin/perl
+
+package Test::TempDir;
+
+use strict;
+use warnings;
+
+use File::Temp ();
+
+use Test::TempDir::Factory;
+
+use Sub::Exporter -setup => {
+	exports => [qw(temp_root tempdir tempfile scratch)],
+	groups => {
+		default => [qw(temp_root tempdir tempfile)],
+	},
+};
+
+our ( $factory, $dir );
+
+sub _factory   { $factory ||= Test::TempDir::Factory->new }
+sub _dir       { $dir     ||= _factory->create }
+
+END { undef $dir; undef $factory };
+
+sub temp_root () { _dir->dir }
+
+sub _temp_args { DIR => temp_root()->stringify, CLEANUP => 0 }
+sub _template_args {
+	if ( @_ % 2 == 0 ) {
+		return ( _temp_args, @_ );
+	} else {
+		return ( $_[0], _temp_args, @_[1 .. $#_] );
+	}
+}
+
+sub tempdir { File::Temp::tempdir( _optional_template_args(@_) ) }
+
+sub tempfile { File::Temp::tempfile( _template_args(@_) ) }
+
+sub scratch {
+	require Directory::Scratch;
+	my $d = Directory::Scratch->new( _temp_args, @_ );
+	$d->{CLEANUP} = 1; # make it stfu, we know what we're doing
+	return $d;
+}
+
+
+__PACKAGE__
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Test::TempDir - 
+
+=head1 SYNOPSIS
+
+	use Test::TempDir qw(tempdir tempfile scratch);
+
+=head1 DESCRIPTION
+
+=cut
+
+
---
+++ t/basic.t
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Test::More 'no_plan';
+
+use ok 'Test::TempDir' => qw(temp_root scratch tempfile);
+
+isa_ok( my $root = temp_root, "Path::Class::Dir" );
+
+ok( my ( $fh, $file ) = tempfile(), "tempfile" );
+
+ok( $fh, "file handle returned" );
+ok( $file, "file name returned" );
+
+ok( ref($fh), "filehandle is a ref" );
+ok( eval { fileno($fh) }, "file opened" );
+ok( (print $fh "bar"), "writable" );;
+
+ok( !ref($file), "file name is not a ref" );
+ok( -f $file, "file exists" );
+
+ok( $root->contains($file), "root contains file" );
+
+SKIP: {
+	skip "no Directory::Scratch", 2 unless eval { require Directory::Scratch };
+
+	isa_ok( my $s = scratch(), "Directory::Scratch" );
+
+	ok( $root->contains($s->base), "root contains scratch dir" );
+}
+
